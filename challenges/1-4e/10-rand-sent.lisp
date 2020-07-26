84fca7d77226a0034fb5bcc9679225523ef2ef81
Konstantine <konstantinev@protonmail.com>
2020-07-25 21:29:40 -0500

    updated random sentance gen to work correctly

--- challenges/1-4e/10-rand-sent.lisp
+++ challenges/1-4e/10-rand-sent.lisp
@@ -1,19 +1,17 @@
 (defparameter word-nouns `("you" "we" "him" "her" "they"))
 (defparameter word-verbs `("were" "been" "had" "done" "said" "talk" "walk"))
 (defparameter word-other `("to" "for" "from" "and"))
-(defparameter random-var 0)
-(defparameter next-words "")
-(defparameter final-sent `(""))
 
 (defun rand-sentence (times)
-  (loop for x upto (- times 1) do
-    (setq random-var (random 3))
-    (cond
-      ((= random-var 0) (setq next-words (nth (random (length word-nouns)) word-nouns)))
-      ((= random-var 1) (setq next-words (nth (random (length word-verbs)) word-verbs)))
-      ((= random-var 2) (setq next-words (nth (random (length word-other)) word-other))))
-    (push next-words final-sent))
-  (format nil "~{~a~^ ~}" final-sent))
+  (let* ((next-words "")(final-sent `("")))
+    (loop for x upto (- times 1) do
+      (let ((random-var (random 3)))
+        (cond
+          ((= random-var 0) (setq next-words (nth (random (length word-nouns)) word-nouns)))
+          ((= random-var 1) (setq next-words (nth (random (length word-verbs)) word-verbs)))
+          ((= random-var 2) (setq next-words (nth (random (length word-other)) word-other)))))
+      (push next-words final-sent))
+    (format nil "~{~a~^ ~}" final-sent)))
 
 ;; Notes
 ;; There's a bug which I can't seem to figure out and that's when you rerun the function in the REPL
